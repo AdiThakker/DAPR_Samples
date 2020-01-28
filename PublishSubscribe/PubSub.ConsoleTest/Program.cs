45eecca776ec9a974629c0fbf52ee7a9e5e3730e
Aditya Thakker <aditya.thakker@avanade.com>
2020-01-28 13:44:43 -0500

    Basic scaffolding for the PubSub Id Generation

---
+++ PublishSubscribe/PubSub.ConsoleTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PubSub.ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
